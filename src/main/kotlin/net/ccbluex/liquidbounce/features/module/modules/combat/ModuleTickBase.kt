5f6ddb6e71029b617a2b0f53bede9b09f81d1e57
BerryRock0 <mdenials128@gmail.com>
2026-06-20 08:56:10 +0000

    Update ModuleTickBase.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTickBase.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleTickBase.kt
@@ -58,17 +58,16 @@ internal object ModuleTickBase : ClientModule("TickBase", ModuleCategories.COMBA
      * The range defines where we want to tickbase into. The first value is the minimum range, which we can
      * tick into, and the second value is the range where we cannot tickbase at all.
      */
-    private val range by floatRange("Range", 2.5f..4f, 0f..8f)
+    private val range by floatRange("Range", 2.5f..4f, 0f..10f)
 
-    private val balanceRecoveryIncrement by float("BalanceRecoverIncrement", 1f, 0f..2f)
-    private val balanceMaxValue by int("BalanceMaxValue", 20, 0..200)
-    private val maxTicksAtATime by int("MaxTicksAtATime", 4, 1..20, "ticks")
+    private val balanceRecoveryIncrement by float("BalanceRecoverIncrement", 1f, 0f..10f)
+    private val balanceMaxValue by int("BalanceMaxValue", 20, 0..1000)
+    private val maxTicksAtATime by int("MaxTicksAtATime", 4, 0..20, "ticks")
     private val pauseOnFlag by boolean("PauseOfFlag", true)
     private val pause by int("Pause", 0, 0..20, "ticks")
-    private val cooldown by int("Cooldown", 0, 0..100, "ticks")
+    private val cooldown by int("Cooldown", 0, 0..1200, "ticks")
     private val forceGround by boolean("ForceGround", false)
-    private val lineColor by color("Line", Color4b.WHITE)
-        .doNotIncludeAlways()
+    private val lineColor by color("Line", Color4b.WHITE).doNotIncludeAlways()
 
     private val requiresKillAura by boolean("RequiresKillAura", true)
 
