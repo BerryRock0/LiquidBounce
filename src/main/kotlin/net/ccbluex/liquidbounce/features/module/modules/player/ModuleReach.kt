ccae1a4e641942ce2296d6b990a3094f48b7007d
BerryRock0 <mdenials128@gmail.com>
2026-06-20 08:44:08 +0000

    Update ModuleReach.kt

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleReach.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/player/ModuleReach.kt
@@ -32,5 +32,5 @@ import net.ccbluex.liquidbounce.utils.range.RangeValueGroup
  */
 object ModuleReach : ClientModule("Reach", ModuleCategories.PLAYER) {
     val entity = tree(RangeValueGroup("Entity", 1f, 0f))
-    val blockRangeIncrease by float("BlockRangeIncrease", 0.5f, 0f..64f)
+    val blockRangeIncrease by float("BlockRangeIncrease", 0.5f, 0f..100f)
 }
